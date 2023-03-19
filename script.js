6319ccd239506ec45d5d63c5b9f920d16a72266e
simon.horrabin <simonhorrabin@gmail.com>
2023-03-19 14:35:43 -0400

    html & css

---
+++ script.js
@@ -0,0 +1,35 @@
+const timeE1 = document.getElementById('time');
+const dateE1 = document.getElementById('date');
+const currentWeatherItemsE1 = document.getElementById
+('current-weather-items');
+const timezone = document.getElementById('time-zone');
+const countryE1 = document.getElementById('country');
+const weatherForecastE1 =document.getElementById('weather-forecast');
+const currentTempE1 = document.getElementById('current-temp');
+
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+const API_Key = '90ccaf76a1c136daef345d47598c58e1'
+
+setInterval(() => {
+const time = new Date();
+const month = time.getMonth();
+const date = time.getDate
+const day = time.getDay();
+const hour = time.getHours();
+const hoursIn12HrFormat = hour >= 13 ? %12: hour
+const minutes = time.getMinutes();
+const ampm = hours >=12 ? 'PM' : 'AM'
+
+timeE1.innerHTML = hoursIn12HrFormat + ':'minutes+ ' ' + <span id="am-pm">${ampm}</span>'
+
+dateE1.innerHTML = days[day] + ' , ' +  date+ ' ' + months[month] 
+
+}, 1000);
+
+function getWeatherData ( {
+    navigator.geolocation.getCurrentPosition((success) => {
+        console.log(success);
+    })
+})
